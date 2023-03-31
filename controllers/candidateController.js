71c331b05c7ede9dde39d1b1c4d4222121213374
Eldar Rakhimov <midnightdidik@itiva.ru>
2023-03-31 19:23:02 +0300

    Added an update candidate function

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -27,7 +27,23 @@ class CandidateController {
     }
     async updateOne(req, res, next) {
         try {
-        } catch (e) {}
+            const candidateId = req.params.id
+            const {name, surname, client, is_paid, userId} = req.body
+            const candidate = await Candidate.findByPk(candidateId)
+            if (candidate) {
+                candidate.name = name,
+                candidate.surname = surname,
+                candidate.client = client,
+                candidate.is_paid = is_paid,
+                candidate.userId = userId
+                await candidate.save()
+                res.json(candidate)
+            } else {
+                res.status(404).send('User not found')  // change to custom middleware
+            }
+        } catch (e) {
+            res.json(e.message)
+        }
     }
     async deleteOne(req, res, next) {
         try {

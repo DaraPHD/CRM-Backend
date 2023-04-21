e8a52174d5e102e043dc38bba4f3bd5a9a904728
Eldar Rakhimov <midnightdidik@itiva.ru>
2023-04-21 12:17:59 +0300

    Added credentials for cors policy

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const errorMiddleware = require("./middlewares/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 app.use(fileUpload({}))
 app.use(cookieParser());

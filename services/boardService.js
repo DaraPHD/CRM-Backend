743a333c4f52f835e63654cd9f109db09fe11b4c
Eldar Rakhimov <midnightdidik@itiva.ru>
2023-07-07 15:51:04 +0300

    added userBoard create after confirm invintation

--- services/boardService.js
+++ services/boardService.js
@@ -1,4 +1,4 @@
-const { Board, Column, Card, Label } = require("../models/models");
+const { Board, Column, Card, Label, UserBoard } = require("../models/models");
 const { Op } = require("sequelize");
 const nodemailer = require("nodemailer");
 
@@ -65,6 +65,54 @@ class BoardService {
             return "Ошибка получения Board";
         }
     }
+    async sendInvintation(id, senderUser, receiverUser, boardName) {
+        try {
+            await this.getBoard(id);
+            await UserBoard.create({
+                userId: receiverUser.id,
+                boardId: id,
+            });
+            const boardURL = "http://10.66.66.20:3001/boards/" + id;
+            const invintationMessage = `
+            <div style="text-align: center; padding: 10px;">
+            <div style="display: inline-block; border: 1px solid #000000; padding: 10px; background-color: #f0f0f0;">
+            <div style="background: linear-gradient(to right, #007bff, #00ffff); display: inline-block; padding: 5px 10px; border-radius: 5px;">
+            <span style="color: #ffffff; font-weight: bold;">RONOVE</span>
+            </div><br>
+            Вы были приглашены пользователем ${senderUser.name} ${senderUser.surname} к доске ${boardName}.<br>
+            Присоединяйтесь для совместной работы.<br>
+            <div style="display: inline-block; margin-top: 20px;">
+            <a href="${boardURL}" style="display: inline-block; background: linear-gradient(to right, #007bff, #00ffff); color: #FFFFFF; padding: 10px 20px; border-radius: 10px; text-decoration: none;">
+            Принять приглашение
+            </a>
+            </div>
+            </div>
+            </div>
+            `;
+            const mailOptions = {
+                from: "midnightdidik@gmail.com",
+                to: receiverUser.email,
+                subject: "Приглашение к доске",
+                html: invintationMessage,
+            };
+            await transporter.sendMail(mailOptions);
+            return "Invitation sent successfully";
+        } catch (e) {
+            console.log("Error sending invitation:", e);
+            throw new Error("Error sending invitation");
+        }
+    }
+
+    // {
+    //     "boardId": "12345",
+    //     "senderUser": {
+    //         "name": "John Doe",
+    //     },
+    //     "receiverUser": {
+    //         "email": "receiver@example.com",
+    //         "id": "id"
+    //     },
+    // };
 
     async searchCard(title) {
         try {
@@ -134,49 +182,6 @@ class BoardService {
             return "Ошибка поиска";
         }
     }
-
-    async sendInvintation(id, senderUser, receiverUser, boardName) {
-        try {
-            await this.getBoard(id);
-            const boardURL = "http://10.66.66.20:3001/boards/" + id;
-            const invintationMessage = `
-            <div style="text-align: center; padding: 10px;">
-            <div style="display: inline-block; border: 1px solid #000000; padding: 10px; background-color: #f0f0f0;">
-            <div style="background: linear-gradient(to right, #007bff, #00ffff); display: inline-block; padding: 5px 10px; border-radius: 5px;">
-            <span style="color: #ffffff; font-weight: bold;">RONOVE</span>
-            </div><br>
-            Вы были приглашены пользователем ${senderUser.name} ${senderUser.surname} к доске ${boardName}.<br>
-            Присоединяйтесь для совместной работы.<br>
-            <div style="display: inline-block; margin-top: 20px;">
-            <a href="${boardURL}" style="display: inline-block; background: linear-gradient(to right, #007bff, #00ffff); color: #FFFFFF; padding: 10px 20px; border-radius: 10px; text-decoration: none;">
-            Принять приглашение
-            </a>
-            </div>
-            </div>
-            </div>
-            `;
-            const mailOptions = {
-                from: "midnightdidik@gmail.com",
-                to: receiverUser.email,
-                subject: "Приглашение к доске",
-                html: invintationMessage,
-            };
-            await transporter.sendMail(mailOptions);
-            return "Invitation sent successfully";
-        } catch (e) {
-            console.log("Error sending invitation:", e);
-            throw new Error("Error sending invitation");
-        }
-    }
-    // {
-    //     "boardId": "12345",
-    //     "senderUser": {
-    //         "name": "John Doe",
-    //     },
-    //     "receiverUser": {
-    //         "email": "receiver@example.com",
-    //     },
-    // };
 
     async delete(id) {
         try {
